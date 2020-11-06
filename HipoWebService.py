8327c6f27bf27c0904b15a35f691332ef86b6a19
f1se4 <52441535+f1se4@users.noreply.github.com>
2020-11-06 10:03:23 +0100

    cache

--- HipoWebService.py
+++ HipoWebService.py
@@ -2,6 +2,19 @@ from flask import render_template, request, Flask
 from HipoChart import f_make_hipo_chart, f_make_dataframe, f_validaciones
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 @app.route('/', methods=["GET", "POST"])
 def home_page():
